5e5b31a4b4938d887b0ed6c671d51284e30b879d
Savvas Georgiou <savvas@dailysecret.com>
2014-04-08 12:20:37 +0100

    Initial commit - Rails 4 application - posgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Basekto::Application.config.secret_key_base = 'cf99fbe05ce4db3641d587092008a6b12cdd4495a7d136a74f46b8422597fc33f9e418f46ab040354fa34188f22ccb8592e9697721d0aac8d3d65ac1e0b5386c'
