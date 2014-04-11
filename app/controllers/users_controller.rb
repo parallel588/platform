8186000719f8efbc10aa482c7b28422e282b7133
Savvas Georgiou <savvas@dailysecret.com>
2014-04-11 06:21:45 +0300

    Added user edit form page and confirmed the basic functionality - save and update - with tests.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,27 @@ class UsersController < ApplicationController
   
   
   def edit
-    
+    @user = current_user
   end
   
   
   def update
+    # Rekect the password values when the password is not on change
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+    end
     
+    @user = current_user
+    @user.update_attributes(params[:user])
+    
+    if @user.save!
+      flash[:notice] = t('user.preferences_successfully_saved')
+      redirect_to "/"
+    else
+      flash[:alert] = t('user.preferences_not_saved')
+      redirect_to user_prefernces_path
+    end
   end
   
 end
--- spec/requests/user/user_preferences_spec.rb
+++ spec/requests/user/user_preferences_spec.rb
@@ -6,15 +6,28 @@ describe "UserPreferences" do
   
     
   describe "Test the user preferences form actions" do
+    it "Redirects the user to the login page when he is not logged in " do
+      visit "/preferences"
+      current_path.should == new_user_session_path                          
+    end
+    
     it "Loading the preferences page with the all the user form fields filled" do
       login_as(user, :scope => :user)            
       visit "/preferences"
-      
+      find_field('Name').value == user.name
+      find_field('Occupation').value == user.occupation
+      find_button('Update preferences')
     end
     
     it "Update the name and occupation and confirm" do
       login_as(user, :scope => :user)            
       visit "/preferences"      
+      fill_in 'Name', :with => "New Name"
+      fill_in 'Occupation', :with => "New Occupation"
+      click_on ('Update preferences')
+      
+      current_path.should == "/"
+      
     end
     
     it "Update the password of the user" do
