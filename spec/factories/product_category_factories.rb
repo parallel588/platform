9d07f853b6f5ad37519ef20a61c083a81de27ddf
Savvas Georgiou <savvas@dailysecret.com>
2014-04-16 14:39:44 +0300

    Fixed spec for products.

--- spec/factories/product_category_factories.rb
+++ spec/factories/product_category_factories.rb
@@ -2,9 +2,7 @@
 FactoryGirl.define do
 
   factory :product_category do
-    sequence(:name) { |n| "Fruits" }    
-    p_category = FactoryGirl.create(:product_category)
-    p_subcategory = FactoryGirl.create(:product_category, { name: "Strawberries", parent: p_category})    
+    sequence(:name) { |n| "Fruit #{n}" }    
   end
   
   
