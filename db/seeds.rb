84b17d9ae36f99476c15fb2273cfa733b9f0eee6
Savvas Georgiou <savvas@dailysecret.com>
2014-06-12 16:02:51 +0300

    Added a draft seed file filling products, categories and auctions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+CATEGORY_NAMES = ["Fruits", "Vegetables", ""]
+SUBCATEGORY_NAMES = [["Apples", "Oranges", "Strawberies"], ["Tomatos", "Potatos", "Cucumbers"]]
+
+
+CATEGORY_NAMES.each_with_index.do do |category_name, i|
+    category = Category.find_or_create(name: category_name)
+    SUBCATEGORY_NAMES[i].each do |subcategory| 
+      Category.create(name: subcategory, parent: category)
+    end
+end
+
+
+# Create Products with Auctions
+6.times do |i|
+  product = Product.create({
+    name: "Product ##{i}", 
+    description: "The ##{i} product created by seed data.",    
+  })
+  auction = Auction.create({
+    product: product,
+    starting_at: Time.now - i.days,
+    ending_at: Time.now + i.days + i.months
+    starting_bid: 5*i+1
+    bid_increment: i+1
+    buy_out_bid: i%2 == 0 ? nil : 10*i
+  })
+end
+
+Auction.all.each do |auction|
+  Bidding.create({
+    auction: auction,
+    
+    
+  })
+end
