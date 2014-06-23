e411ab92c9f76ed36efb093db38806e4c2194563
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2014-06-24 00:20:41 +0200

    One implementation of a JS timer script.

--- app/assets/javascripts/application.config.js
+++ app/assets/javascripts/application.config.js
@@ -9,6 +9,7 @@ requirejs.config({
     "backbone.wreqr": "bower_components/backbone.wreqr/lib/amd/backbone.wreqr",
     "skrollr": "libs/skrollr",
     "waypoints": "libs/waypoints",
-    "owl.carousel": "bower_components/OwlCarousel/owl-carousel/owl.carousel"
+    "owl.carousel": "bower_components/OwlCarousel/owl-carousel/owl.carousel",
+    "timecircles": "libs/TimeCircles"
   }
 });
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,8 +3,23 @@ require([
   "jquery", 
   "backbone", 
   "waypoints",
+  "timecircles",
   "rails_dependencies"
 ], function(app, $) {
+
+  $("#timecircle").TimeCircles({ 
+    count_past_zero: false,
+    circle_bg_color: "#8DBF44",
+    fg_width: 0.2,
+    bg_width: 0.1,
+    direction: "counterClockwise",
+    time: {
+      Days: { show: false },
+      Hours: { show: false },
+      Minutes: { color: "white" },
+      Seconds: { show: false }
+    }
+  });
 
   console.log("load app js");
 
