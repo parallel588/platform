4f01ad7c6ca7d7f1b3a5b990095b9e59ae43b0b5
Savvas Georgiou <savvas@dailysecret.com>
2014-07-07 01:18:50 +0300

    Make billing mandatory for a proper seller user profile. Added before-filter in bidding actions to prevent biddings from profiles without billing information.

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -1,16 +1,15 @@
 class BiddingsController < ApplicationController
   before_action :authenticate_user!
-			
+  before_action :get_auction, :except => [:destroy]     
+  before_action :ensure_user_can_place_bidding!, :only => [:create]			
 
 
 
   def new
-  	@auction = Auction.where("id = ?", params[:auction_id]).includes(:product).first
   end
 
 
   def create
-    @auction = Auction.where("id = ?", params[:auction_id]).includes(:product).first
   	bidding =  Bidding.new( {amount: params[:bidding][:amount], user: current_user,  auction: @auction,  product: @auction.product} )
   	if bidding.save
   		flash[:notice] = t('biddings.your_bidding_is_successfully_submitted')
@@ -33,9 +32,26 @@ class BiddingsController < ApplicationController
   
   
   def mark_as_winner
-    @auction = Auction.where("id = ?", params[:auction_id]).first
     @bidding = Bidding.where("id = ?", params[:bidding_id]).first    
     @bidding.make_winner!
     redirect_to seller_dashboard_url
   end
+
+  
+
+  private
+
+  def ensure_user_can_place_bidding!
+    if !current_user.billings.first.blank? && current_user.billings.first.valid?
+      return true
+    else
+      flash[:warning] = t('user.please_add_billing_infomation')
+      redirect_to user_prefernces_path
+    end
+  end
+
+  def get_auction
+    @auction = Auction.where("id = ?", params[:auction_id]).includes(:product).first    
+  end
+
 end
--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,3 +1,15 @@
 class Billing < ActiveRecord::Base
 	belongs_to :user
+
+  validates :card_number, presence: true
+  validates :expiration_date, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address_line_1, presence: true
+  validates :city, presence: true
+  validates :postal_code, presence: true
+  validates :country, presence: true
+  
+
+  
 end
