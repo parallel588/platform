dd9feb9ff65781812c480ff6e75351f2aac818f8
Savvas Georgiou <savvas@dailysecret.com>
2014-08-08 10:43:31 +0300

    Small fix in cropping product images.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,7 +64,7 @@
         $("#crop_w").val(Math.round(c.w * ratio));
         $("#crop_h").val(Math.round(c.h * ratio));
       }
-      $('#product-image-crop').Jcrop({keySupport: false, onSelect: updateCoordinates, minSize: [100, 100]});
+      $('#product-image-crop').Jcrop({keySupport: false, aspectRatio: 1, onSelect: updateCoordinates, minSize: [100, 100]});
     }
   })
 
